c81dc7c141f976b4cc00b449605abfc3686692d9
Joelson Reis <50884925+joelsondsbreis@users.noreply.github.com>
2020-01-25 17:04:43 -0300

    Iniciando CRUD com NodeJS

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const requireDir = require('require-dir');
 
 //Iniciando app
 const app = express();
+app.use(express.json());
 
 //iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { 
